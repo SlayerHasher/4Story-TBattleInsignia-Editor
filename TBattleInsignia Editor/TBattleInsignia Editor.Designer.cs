01875f6f905ebb9134b73acb128756f0e7ddbe4b
SlayerHasher <SlayerHasher@hotmail.com>
2022-04-14 19:57:56 +0400

    PreRelease(1)
    Update Framework to 8.1
    Add button for DB

--- TBattleInsignia Editor/TBattleInsignia Editor.Designer.cs
+++ TBattleInsignia Editor/TBattleInsignia Editor.Designer.cs
@@ -49,8 +49,15 @@ namespace TBattleInsignia_Editor
             this.OFD = new System.Windows.Forms.OpenFileDialog();
             this.SFD = new System.Windows.Forms.SaveFileDialog();
             this.ListBox2 = new System.Windows.Forms.ListBox();
+            this.TcdGroupBox = new System.Windows.Forms.GroupBox();
+            this.DBGroupBox = new System.Windows.Forms.GroupBox();
+            this.connectInDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveDBButton = new System.Windows.Forms.Button();
+            this.SaveAllDBButton = new System.Windows.Forms.Button();
             this.MenuStrip.SuspendLayout();
             this.StatusStrip.SuspendLayout();
+            this.TcdGroupBox.SuspendLayout();
+            this.DBGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // MenuStrip
@@ -58,6 +65,7 @@ namespace TBattleInsignia_Editor
             this.MenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.FileToolStripMenuItem,
+            this.connectInDBToolStripMenuItem,
             this.AboutToolStripMenuItem});
             this.MenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MenuStrip.Name = "MenuStrip";
@@ -77,14 +85,14 @@ namespace TBattleInsignia_Editor
             // OpenToolStripMenuItem
             // 
             this.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem";
-            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.OpenToolStripMenuItem.Text = "Open";
             this.OpenToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
             // 
             // SaveToolStripMenuItem
             // 
             this.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem";
-            this.SaveToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.SaveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.SaveToolStripMenuItem.Text = "Save";
             this.SaveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
             // 
@@ -159,15 +167,15 @@ namespace TBattleInsignia_Editor
             this.PointLabel.AutoSize = true;
             this.PointLabel.Location = new System.Drawing.Point(217, 71);
             this.PointLabel.Name = "PointLabel";
-            this.PointLabel.Size = new System.Drawing.Size(40, 17);
+            this.PointLabel.Size = new System.Drawing.Size(37, 16);
             this.PointLabel.TabIndex = 5;
             this.PointLabel.Text = "Point";
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(220, 257);
+            this.SaveButton.Location = new System.Drawing.Point(6, 50);
             this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(189, 23);
+            this.SaveButton.Size = new System.Drawing.Size(178, 23);
             this.SaveButton.TabIndex = 6;
             this.SaveButton.Text = "Save";
             this.SaveButton.UseVisualStyleBackColor = true;
@@ -175,9 +183,9 @@ namespace TBattleInsignia_Editor
             // 
             // AddButton
             // 
-            this.AddButton.Location = new System.Drawing.Point(220, 228);
+            this.AddButton.Location = new System.Drawing.Point(6, 21);
             this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(92, 23);
+            this.AddButton.Size = new System.Drawing.Size(88, 23);
             this.AddButton.TabIndex = 7;
             this.AddButton.Text = "Add";
             this.AddButton.UseVisualStyleBackColor = true;
@@ -185,9 +193,9 @@ namespace TBattleInsignia_Editor
             // 
             // DeleteButton
             // 
-            this.DeleteButton.Location = new System.Drawing.Point(317, 228);
+            this.DeleteButton.Location = new System.Drawing.Point(96, 21);
             this.DeleteButton.Name = "DeleteButton";
-            this.DeleteButton.Size = new System.Drawing.Size(92, 23);
+            this.DeleteButton.Size = new System.Drawing.Size(88, 23);
             this.DeleteButton.TabIndex = 8;
             this.DeleteButton.Text = "Delete";
             this.DeleteButton.UseVisualStyleBackColor = true;
@@ -208,15 +216,61 @@ namespace TBattleInsignia_Editor
             this.ListBox2.Visible = false;
             this.ListBox2.SelectedIndexChanged += new System.EventHandler(this.ListBox2_SelectedIndexChanged);
             // 
+            // TcdGroupBox
+            // 
+            this.TcdGroupBox.Controls.Add(this.SaveButton);
+            this.TcdGroupBox.Controls.Add(this.AddButton);
+            this.TcdGroupBox.Controls.Add(this.DeleteButton);
+            this.TcdGroupBox.Location = new System.Drawing.Point(220, 200);
+            this.TcdGroupBox.Name = "TcdGroupBox";
+            this.TcdGroupBox.Size = new System.Drawing.Size(190, 80);
+            this.TcdGroupBox.TabIndex = 10;
+            this.TcdGroupBox.TabStop = false;
+            this.TcdGroupBox.Text = "Tcd";
+            // 
+            // DBGroupBox
+            // 
+            this.DBGroupBox.Controls.Add(this.SaveAllDBButton);
+            this.DBGroupBox.Controls.Add(this.SaveDBButton);
+            this.DBGroupBox.Location = new System.Drawing.Point(220, 143);
+            this.DBGroupBox.Name = "DBGroupBox";
+            this.DBGroupBox.Size = new System.Drawing.Size(190, 51);
+            this.DBGroupBox.TabIndex = 11;
+            this.DBGroupBox.TabStop = false;
+            this.DBGroupBox.Text = "DB";
+            // 
+            // connectInDBToolStripMenuItem
+            // 
+            this.connectInDBToolStripMenuItem.Name = "connectInDBToolStripMenuItem";
+            this.connectInDBToolStripMenuItem.Size = new System.Drawing.Size(117, 24);
+            this.connectInDBToolStripMenuItem.Text = "Connect in DB";
+            // 
+            // SaveDBButton
+            // 
+            this.SaveDBButton.Location = new System.Drawing.Point(6, 21);
+            this.SaveDBButton.Name = "SaveDBButton";
+            this.SaveDBButton.Size = new System.Drawing.Size(88, 23);
+            this.SaveDBButton.TabIndex = 0;
+            this.SaveDBButton.Text = "Save";
+            this.SaveDBButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveAllDBButton
+            // 
+            this.SaveAllDBButton.Location = new System.Drawing.Point(96, 21);
+            this.SaveAllDBButton.Name = "SaveAllDBButton";
+            this.SaveAllDBButton.Size = new System.Drawing.Size(88, 23);
+            this.SaveAllDBButton.TabIndex = 1;
+            this.SaveAllDBButton.Text = "Save All";
+            this.SaveAllDBButton.UseVisualStyleBackColor = true;
+            // 
             // TBattleInsignia_Editor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(421, 318);
+            this.Controls.Add(this.DBGroupBox);
+            this.Controls.Add(this.TcdGroupBox);
             this.Controls.Add(this.ListBox2);
-            this.Controls.Add(this.DeleteButton);
-            this.Controls.Add(this.AddButton);
-            this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.PointLabel);
             this.Controls.Add(this.PointTextBox);
             this.Controls.Add(this.StatusStrip);
@@ -234,6 +288,8 @@ namespace TBattleInsignia_Editor
             this.MenuStrip.PerformLayout();
             this.StatusStrip.ResumeLayout(false);
             this.StatusStrip.PerformLayout();
+            this.TcdGroupBox.ResumeLayout(false);
+            this.DBGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -260,6 +316,11 @@ namespace TBattleInsignia_Editor
         private System.Windows.Forms.OpenFileDialog OFD;
         private System.Windows.Forms.SaveFileDialog SFD;
         private System.Windows.Forms.ListBox ListBox2;
+        private System.Windows.Forms.GroupBox TcdGroupBox;
+        private System.Windows.Forms.GroupBox DBGroupBox;
+        private System.Windows.Forms.ToolStripMenuItem connectInDBToolStripMenuItem;
+        private System.Windows.Forms.Button SaveAllDBButton;
+        private System.Windows.Forms.Button SaveDBButton;
     }
 }
 
